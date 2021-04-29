0a93e6ed43f56e6817fb75999da2be255d8e91ec
Arnold Schelb <ardrift@gmail.com>
2021-04-29 17:20:34 +0200

    Remove field ArrayList remains

--- src/snake/Field.java
+++ src/snake/Field.java
@@ -1,28 +1,21 @@
 package snake;
-// import java.util.ArrayList;
 
 public class Field {
     private int width;
     private int height;
     private Cell[][] matrix;
-    //private ArrayList<ArrayList<Cell>> matrix = new ArrayList<>();
 
     public Field(int w, int h) {
         width = w;
         height = h;
         setMatrix(w, h);
         for (int i = 0; i < w; i++) {
-            //getMatrix().add(new ArrayList<Cell>());
             for (int j = 0; j < h; j++) {
-                //getMatrix().get(i).add(new Cell('e'));
                 getMatrix()[i][j] = new Cell('e');
             }
         }
     }
 
-/*    public ArrayList<ArrayList<Cell>> getMatrix() {
-        return matrix;
-    }*/
     public Cell[][] getMatrix() {
         return matrix;
     }
@@ -32,17 +25,13 @@ public class Field {
     }
 
     public void setCell(int x, int y, char c) {
-        //getMatrix().get(x).get(y).setType(c);
         getMatrix()[x][y].setType(c);
     }
 
     public void print() {
-        //for (int i = 0; i < getMatrix().get(0).size(); i++) {
         for (int i = 0; i < getMatrix()[0].length; i++) {
-            //for (int j = 0; j < getMatrix().size(); j++) {
             for (int j = 0; j < getMatrix().length; j++) {
                 System.out.print(j + "." + i + ": "
-//                                + getMatrix().get(j).get(i).getType() + "  ");
                                   + getMatrix()[j][i].getType() + "  ");
             }
             System.out.println("");
