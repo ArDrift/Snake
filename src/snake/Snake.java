7defa7ddd8f2292d5ccbe4746c31bf5ce9607904
Arnold Schelb <ardrift@gmail.com>
2021-05-01 16:45:51 +0200

    Fix snake types

--- src/snake/Snake.java
+++ src/snake/Snake.java
@@ -2,10 +2,10 @@ package snake;
 
 public class Snake {
     private int length;
-    private Char dir;
+    private char dir;
     private int[] pos = new int[2];
 
-    public Snake(int px, int py, int d) {
+    public Snake(int px, int py, char d) {
         pos[0] = px;
         pos[1] = py;
         dir = d;
